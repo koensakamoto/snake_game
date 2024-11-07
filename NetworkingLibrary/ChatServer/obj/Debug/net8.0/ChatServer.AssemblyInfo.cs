0ec91ea1a713dc64cca291cafa7cf2064232660d
dom-becs <u1452229@umail.utah.edu>
2024-11-07 15:02:53 -0700

    all fucntionality achieved

--- NetworkingLibrary/ChatServer/obj/Debug/net8.0/ChatServer.AssemblyInfo.cs
+++ NetworkingLibrary/ChatServer/obj/Debug/net8.0/ChatServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09f26591e35eb5a2d3c42bf20c1449ca0f72d114")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff9a7c37419a8f57a9a5cb520dc485e908034322")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
